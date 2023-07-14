7bd64744f61eed9c36b9e7c3d28787530883f395
jsavantika <jain.avantikasikand@gmail.com>
2023-07-14 11:13:43 +0100

    Created separate handler class

--- JtProject/src/main/java/com/jtspringproject/JtSpringProject/JtSpringProjectApplication.java
+++ JtProject/src/main/java/com/jtspringproject/JtSpringProject/JtSpringProjectApplication.java
@@ -12,10 +12,3 @@ public class JtSpringProjectApplication {
 	}
 
 }
-public class LambdaRequestHandler
-  implements RequestHandler<String, String> {
-    public String handleRequest(String input, Context context) {
-        context.getLogger().log("Input: " + input);
-        return "Hello World - " + input;
-    }
-}
---
+++ JtProject/src/main/java/com/jtspringproject/JtSpringProject/LambdaRequestHandler.java
@@ -0,0 +1,13 @@
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class LambdaRequestHandler
+  implements RequestHandler<String, String> {
+    public String handleRequest(String input, Context context) {
+        context.getLogger().log("Input: " + input);
+        return "Hello World - " + input;
+    }
+}
